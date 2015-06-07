d3642ed8d46b63ca4b4d09032a3e75a1f50d17cc
vram4 <helloimpie4@gmail.com>
2015-06-06 19:57:37 -0700

    Optimized sending leaderboard packets
    Leaderboard packets are now built once per interval instead of being
    rebuilt for each individual player

--- src/packet/UpdateLeaderboard.js
+++ src/packet/UpdateLeaderboard.js
@@ -13,6 +13,50 @@ UpdateLeaderboard.prototype.build = function() {
     
     switch (this.packetLB) {
         case 48: // Custom Text List
+        	// Get size of packet            
+            for (var i = 0; i < lb.length; i++) {
+            	
+                if (typeof lb[i] == "undefined") {
+                    continue;
+                }
+
+                var item = lb[i];
+                bufferSize += 4; // Empty ID
+                bufferSize += item.length * 2 ; // String length
+                bufferSize += 2; // Name terminator
+
+                validElements++;
+            }       
+            
+            var buf = new ArrayBuffer(bufferSize);
+            var view = new DataView(buf);
+            
+            // Set packet data
+            view.setUint8(0, 49, true); // Packet ID
+            view.setUint32(1, validElements, true); // Number of elements
+            var offset = 5;
+            
+            // Loop through strings
+            for (var i = 0; i < lb.length; i++) {
+
+                if (typeof lb[i] == "undefined") {
+                    continue;
+                }
+
+                var item = lb[i];
+                
+                view.setUint32(offset, 1, true);
+                offset += 4;
+                
+                for (var j = 0; j < name.length; j++) {
+                    view.setUint16(offset, name.charCodeAt(j), true);
+                    offset += 2;
+                }
+
+                view.setUint16(offset, 0, true);
+                offset += 2;
+            }
+            return buf;
             break;
         case 49: // FFA-type Packet (List)
             // Get size of packet            
