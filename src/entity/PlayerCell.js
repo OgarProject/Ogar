b70a0f876d7ac27352f80972f148a04619fb71e0
Chris Moore <dooglus@gmail.com>
2015-06-12 15:36:05 -0700

    Apply formatting rules.

--- src/entity/PlayerCell.js
+++ src/entity/PlayerCell.js
@@ -13,37 +13,37 @@ PlayerCell.prototype = new Cell();
 
 PlayerCell.prototype.visibleCheck = function(box,centerPos) {
     // Use old fashioned checking method if cell is small
-	if (this.mass < 150) {
-	    return this.collisionCheck(box.bottomY,box.topY,box.rightX,box.leftX);
-	}
-	
-	// Checks if this cell is visible to the player
+    if (this.mass < 150) {
+        return this.collisionCheck(box.bottomY,box.topY,box.rightX,box.leftX);
+    }
+
+    // Checks if this cell is visible to the player
     var len = this.getSize() + box.width >> 0; // Width of cell + width of the box (Int)
-    
-    return ((this.position.x - centerPos.x) < len) && 
+
+    return ((this.position.x - centerPos.x) < len) &&
            ((this.position.y - centerPos.y) < len);
-}
+};
 
 PlayerCell.prototype.calcMergeTime = function(base) {
     this.recombineTicks = base + ((0.02 * this.mass) >> 0); // Int (30 sec + (.02 * mass))
-}
+};
 
 // Override
 
 PlayerCell.prototype.getEatingRange = function() {
     return this.getSize() * .4;
-}
+};
 
 PlayerCell.prototype.onConsume = function(consumer,gameServer) {
     consumer.addMass(this.mass);
-}
+};
 
 PlayerCell.prototype.onAdd = function(gameServer) {
     // Add to special player node list
     gameServer.nodesPlayer.push(this);
     // Gamemode actions
     gameServer.gameMode.onCellAdd(this);
-}
+};
 
 PlayerCell.prototype.onRemove = function(gameServer) {
     var index;
@@ -64,9 +64,9 @@ PlayerCell.prototype.onRemove = function(gameServer) {
     }
     // Gamemode actions
     gameServer.gameMode.onCellRemove(this);
-}
+};
 
 PlayerCell.prototype.moveDone = function(gameServer) {
-	this.setCollisionOff(false);
-}
+    this.setCollisionOff(false);
+};
 
