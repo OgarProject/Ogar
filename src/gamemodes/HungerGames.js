a0ce71dc5a5b3e401461dfca8f9d16db12751340
vram4 <helloimpie4@gmail.com>
2015-06-14 13:07:27 -0700

    Refactor HungerGames.js
    -Fixed timers counting down in 2 second intervals.
    -Hunger games mode is now based on tournament mode

--- src/gamemodes/HungerGames.js
+++ src/gamemodes/HungerGames.js
@@ -1,37 +1,25 @@
-var Mode = require('./Mode');
+var Tournament = require('./Tournament');
 var Entity = require('../entity');
 
 function HungerGames() {
-    Mode.apply(this, Array.prototype.slice.call(arguments));
+    Tournament.apply(this, Array.prototype.slice.call(arguments));
 
     this.ID = 11;
     this.name = "Hunger Games";
-    this.packetLB = 48;
-
-    // Config (1 tick = 2000 ms)
-    this.prepTime = 5; // Amount of ticks after the server fills up to wait until starting the game
-    this.endTime = 15; // Amount of ticks after someone wins to restart the game
-    this.autoFill = false;
-    this.autoFillPlayers = 1;
 
     // Gamemode Specific Variables
-    this.gamePhase = 0; // 0 = Waiting for players, 1 = Prepare to start, 2 = Game in progress, 3 = End
-    this.tributes = [];
-    this.maxTributes = 12;
+    this.maxContenders = 12;
     this.baseSpawnPoints = [
         {x: 1600,y: 200},{x: 3200,y: 200},{x: 4800,y: 200}, // Top
         {x: 200,y: 1600},{x: 200,y: 3200},{x: 200,y: 4800}, // Left
         {x: 6200,y: 1600},{x: 6200,y: 3200},{x: 6200,y: 4800}, // Right
         {x: 1600,y: 6200},{x: 3200,y: 6200},{x: 4800,y: 6200}  // Bottom
     ];
-    this.tributeSpawnPoints;
-
-    this.winner;
-    this.timer;
+    this.contenderSpawnPoints;
 }
 
 module.exports = HungerGames;
-HungerGames.prototype = new Mode();
+HungerGames.prototype = new Tournament();
 
 // Gamemode Specific Functions
 
@@ -39,10 +27,10 @@ HungerGames.prototype.getPos = function() {
     var pos = {x: 0, y: 0};
 
     // Random Position
-    if (this.tributeSpawnPoints.length > 0) {
-        var index = Math.floor(Math.random() * this.tributeSpawnPoints.length);
-        pos = this.tributeSpawnPoints[index];
-        this.tributeSpawnPoints.splice(index,1);
+    if (this.contenderSpawnPoints.length > 0) {
+        var index = Math.floor(Math.random() * this.contenderSpawnPoints.length);
+        pos = this.contenderSpawnPoints[index];
+        this.contenderSpawnPoints.splice(index,1);
     }
 
     return {x: pos.x, y: pos.y};
@@ -60,30 +48,6 @@ HungerGames.prototype.spawnVirus = function(gameServer,pos) {
     gameServer.addNode(v);
 };
 
-HungerGames.prototype.startGamePrep = function(gameServer) {
-    this.gamePhase = 1;
-    this.timer = this.prepTime; // 10 seconds
-};
-
-HungerGames.prototype.startGame = function(gameServer) {
-    gameServer.run = true;
-    this.gamePhase = 2;
-};
-
-HungerGames.prototype.endGame = function(gameServer) {
-    this.winner = this.tributes[0];
-    this.gamePhase = 3;
-    this.timer = this.endTime; // 30 Seconds
-};
-
-HungerGames.prototype.fillBots = function(gameServer) {
-    // Fills the server with bots if there arent enough players
-    var fill = this.maxTributes - this.tributes.length;
-    for (var i = 0;i < fill;i++) {
-        gameServer.bots.addBot();
-    }
-};
-
 // Override
 
 HungerGames.prototype.onServerInit = function(gameServer) {
@@ -98,23 +62,27 @@ HungerGames.prototype.onServerInit = function(gameServer) {
 
         gameServer.removeNode(node);
     }
-
+    
     // Resets spawn points
-    this.tributeSpawnPoints = this.baseSpawnPoints.slice();
+    this.contenderSpawnPoints = this.baseSpawnPoints.slice();
 
     // Pauses the server
     gameServer.run = false;
     this.gamePhase = 0;
 
-    // Override config values
+    // Get config values
     if (gameServer.config.tourneyAutoFill > 0) {
         this.timer = gameServer.config.tourneyAutoFill;
         this.autoFill = true;
         this.autoFillPlayers = gameServer.config.tourneyAutoFillPlayers;
     }
-    if (gameServer.config.serverBots > this.maxTributes) {
-        // The number of bots cannot exceed the maximum amount of tributes
-        gameServer.config.serverBots = this.maxTributes;
+    this.prepTime = gameServer.config.tourneyPrepTime;
+    this.endTime = gameServer.config.tourneyEndTime;
+
+    // Override config values
+    if (gameServer.config.serverBots > this.maxContenders) {
+        // The number of bots cannot exceed the maximum amount of contenders
+        gameServer.config.serverBots = this.maxContenders;
     }
     gameServer.config.spawnInterval = 20;
     gameServer.config.borderLeft = 0;
@@ -173,80 +141,16 @@ HungerGames.prototype.onServerInit = function(gameServer) {
 
 HungerGames.prototype.onPlayerSpawn = function(gameServer,player) {
     // Only spawn players if the game hasnt started yet
-    if ((this.gamePhase == 0) && (this.tributes.length < this.maxTributes)) {
+    if ((this.gamePhase == 0) && (this.contenders.length < this.maxContenders)) {
         gameServer.spawnPlayer(player);
-        this.tributes.push(player); // Add to tribute list
+        this.contenders.push(player); // Add to contenders list
         player.cells[0].position = this.getPos(); // Get random starting position
 
-        if (this.tributes.length == this.maxTributes) {
+        if (this.contenders.length == this.maxContenders) {
             // Start the game once there is enough players
             this.startGamePrep(gameServer);
         }
     }
 };
 
-HungerGames.prototype.onCellRemove = function(cell) {
-    var owner = cell.owner;
-    if (owner.cells.length <= 0) {
-        // Remove from tribute list
-        var index = this.tributes.indexOf(owner);
-        if (index != -1) {
-            this.tributes.splice(index,1);
-        }
-
-        // Victory conditions
-        if ((this.tributes.length == 1) && (this.gamePhase == 2)){
-            this.endGame(cell.owner.gameServer);
-        }
-    }
-};
-
-HungerGames.prototype.updateLB = function(gameServer) {
-    var lb = gameServer.leaderboard;
-
-    switch (this.gamePhase) {
-        case 0:
-            lb[0] = "Waiting for";
-            lb[1] = "players: ";
-            lb[2] = this.tributes.length+"/"+this.maxTributes;
-            if (this.autoFill) {
-                if (this.timer <= 0) {
-                    this.fillBots(gameServer);
-                } else if (this.tributes.length >= this.autoFillPlayers) {
-                    this.timer--;
-                }
-            }
-            break;
-        case 1:
-            lb[0] = "Game starting in";
-            lb[1] = (this.timer * 2).toString();
-            lb[2] = "Good luck!";
-            if (this.timer <= 0) {
-                // Reset the game
-                this.startGame(gameServer);
-            } else {
-                this.timer--;
-            }
-            break;
-        case 2:
-            lb[0] = "Players Remaining";
-            lb[1] = this.tributes.length+"/"+this.maxTributes;
-            break;
-        case 3:
-            lb[0] = "Congratulations";
-            lb[1] = this.winner.getName();
-            lb[2] = "for winning!";
-            if (this.timer <= 0) {
-                // Reset the game
-                this.onServerInit(gameServer);
-                // Respawn starting food
-                gameServer.startingFood();
-            } else {
-                this.timer--;
-            }
-            break;
-        default:
-            break;
-    }
-};
 
