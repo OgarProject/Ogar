13d01952b07918667c1d9a6015550f1f517330a4
Ephemerality <ephemeral.vilification@gmail.com>
2015-07-20 16:07:55 -0300

    Kill player's cells before kicking

--- src/modules/CommandList.js
+++ src/modules/CommandList.js
@@ -176,6 +176,10 @@ Commands.list = {
         for (var i in gameServer.clients) {
             if (gameServer.clients[i].playerTracker.pID == id) {
                 var client = gameServer.clients[i].playerTracker;
+                var len = client.cells.length;
+                for (var j = 0; j < len; j++) {
+                    gameServer.removeNode(client.cells[0]);
+                }
                 client.socket.close();
                 console.log("[Console] Kicked " + client.name);
                 break;
