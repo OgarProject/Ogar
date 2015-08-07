9fa96db0ded004c5a4213f148f3b67a641bd59b7
XCreeperFaceX <nathan.laytart@gmail.com>
2015-08-07 19:53:29 -0400

    127.0.0.1:666

--- src/GameServer.js
+++ src/GameServer.js
@@ -44,7 +44,7 @@ function GameServer() {
     // Config
     this.config = { // Border - Right: X increases, Down: Y increases (as of 2015-05-20)
         serverMaxConnections: 64, // Maximum amount of connections to the server.
-        serverPort: 443, // Server port
+        serverPort: 666, // Server port
         serverGamemode: 0, // Gamemode, 0 = FFA, 1 = Teams
         serverBots: 0, // Amount of player bots to spawn
         serverViewBaseX: 1024, // Base view distance of players. Warning: high values may cause lag
