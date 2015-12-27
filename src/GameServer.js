2f22d7c6001aa26edcd7785286b8af466db8abba
Tiberiu <musattiberiu@gmail.com>
2015-12-27 16:04:25 +0200

    Update GameServer.js

--- src/GameServer.js
+++ src/GameServer.js
@@ -66,6 +66,7 @@ function GameServer() {
         virusStartMass: 100, // Starting virus size (In mass)
         virusFeedAmount: 7, // Amount of times you need to feed a virus to shoot it
         ejectMass: 12, // Mass of ejected cells
+	ejectMassCooldown: 200, // Time until a player can eject mass again
         ejectMassLoss: 16, // Mass lost when ejecting cells
         ejectSpeed: 160, // Base speed of ejected cells
         ejectSpawnPlayer: 50, // Chance for a player to spawn from ejected mass
@@ -627,7 +628,17 @@ GameServer.prototype.splitCells = function(client) {
     }
 };
 
+GameServer.prototype.canEjectMass = function(client){
+    if (typeof client.lastEject == 'undefined' || this.time - client.lastEject >= this.config.ejectMassCooldown){
+	client.lastEject = this.time;
+	return true;
+    } else
+	return false;
+}
+
 GameServer.prototype.ejectMass = function(client) {
+    if (!this.canEjectMass(client))
+	return ;
     for (var i = 0; i < client.cells.length; i++) {
         var cell = client.cells[i];
 
