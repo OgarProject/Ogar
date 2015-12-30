b1506324a3faadf1d9b2f7ec86485a5c5c139bac
Andrew54757 <andrews54757@gmail.com>
2015-12-30 15:53:19 -0500

    Added Fmsg command
    so that it forces people to look at message

--- src/modules/CommandList.js
+++ src/modules/CommandList.js
@@ -53,8 +53,56 @@ Commands.list = {
         console.log("[Console] Merge [id]   Forces that player to merge");
         console.log("[Console] Nojoin [Id]   Prevents the player from merging")
         console.log("[Console] Msg [message 1] [message 2] [etc..] :Sends a message")
+        console.log("[Console] Fmsg [Msg1] [Msg2] [Etc..] forces people to look at msg");
         console.log("[Console] ====================================================");
     },
+    
+    fmsg: function(gameServer,split) {
+        var newLB = [];
+        var n = [];
+        gameServer.run = !gameServer.run; // Switches the pause state
+       
+    
+        for (var i = 1; i < split.length; i++) {
+            newLB[i - 1] = split[i];
+        }   
+        for (var i = 0; i < gameServer.clients.length; i++) {
+                 var client = gameServer.clients[i].playerTracker; 
+            n[i] = client.name;
+            
+            if (client.pID == i+1) {
+                client.name = "Look At Leaderboard";
+            }
+            
+        }
+        gameServer.gameMode.packetLB = 48;
+        gameServer.gameMode.specByLeaderboard = false;
+        gameServer.gameMode.updateLB = function(gameServer) {gameServer.leaderboard = newLB}; 
+        console.log ("[ForceMSG] The message has been broadcast");
+        setTimeout(function() { 
+                         var gm = GameMode.get(gameServer.gameMode.ID);
+        
+        // Replace functions
+        gameServer.gameMode.packetLB = gm.packetLB;
+        gameServer.gameMode.updateLB = gm.updateLB;
+            
+            for (var i = 0; i < gameServer.clients.length; i++) {
+                 var client = gameServer.clients[i].playerTracker; 
+            
+            if (client.pID == i+1) {
+                client.name = n[i];
+            }
+            
+        }
+           gameServer.run = !gameServer.run; 
+            console.log ("[ForceMSG] The game has been reset");
+        } , 6500);
+    },
+                   
+    
+    
+    
+    
     
     msg: function(gameServer,split) {
         var newLB = [];
