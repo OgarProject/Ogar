70c463297379329cf1dcde32321cdbb82b638cc1
Andrew54757 <andrews54757@gmail.com>
2015-12-31 21:26:07 -0500

    Update Food.js

--- src/entity/Food.js
+++ src/entity/Food.js
@@ -6,6 +6,12 @@ function Food() {
     this.cellType = 1;
     this.size = Math.ceil(Math.sqrt(100 * this.mass));
     this.squareSize = (100 * this.mass) >> 0; // not being decayed -> calculate one time
+    this.shouldSendUpdate = false;
+
+    if (this.gameServer.config.foodMassGrow &&
+        Math.floor(Math.random()*101) > this.gameServer.config.foodMassGrowPossiblity) {
+        this.grow();
+    }
 }
 
 module.exports = Food;
@@ -23,10 +29,27 @@ Food.prototype.calcMove = null; // Food has no need to move
 
 // Main Functions
 
+Food.prototype.grow = function() {
+  setTimeout(function () {
+    this.mass ++; // food mass increased, we need to recalculate its size and squareSize, and send update to client side
+    this.size = Math.ceil(Math.sqrt(100 * this.mass));
+    this.squareSize = (100 * this.mass) >> 0;
+    this.shouldSendUpdate = true;
+
+    if (this.mass < this.gameServer.config.foodMassLimit) {
+      this.grow();
+    }
+  }.bind(this), this.gameServer.config.foodMassTimeout * 1000);
+};
+
 Food.prototype.sendUpdate = function() {
     // Whether or not to include this cell in the update packet
     if (this.moveEngineTicks == 0) {
         return false;
+    }
+    if (this.shouldSendUpdate) {
+      this.shouldSendUpdate = false;
+      return true;
     }
     return true;
 };
