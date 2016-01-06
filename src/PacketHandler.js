414d891a13d7fa968d28c15c5cd2996c20dc17eb
scholtes <scholtes.garrett@gmail.com>
2016-01-05 22:53:41 -0500

    Coordinate system obfuscation as an anti-bot measure
    Each client is provided with a different, randomized coordinate system. This thwarts communication between minion bots and a cheating player.

--- src/PacketHandler.js
+++ src/PacketHandler.js
@@ -67,8 +67,8 @@ PacketHandler.prototype.handleMessage = function(message) {
             // Set Target
             if (view.byteLength == 13) {
                 var client = this.socket.playerTracker;
-                client.mouse.x = view.getInt32(1, true);
-                client.mouse.y = view.getInt32(5, true);
+                client.mouse.x = view.getInt32(1, true) - client.scrambleX;
+                client.mouse.y = view.getInt32(5, true) - client.scrambleY;
             }
             break;
         case 17:
@@ -92,7 +92,12 @@ PacketHandler.prototype.handleMessage = function(message) {
                 this.protocol = view.getUint32(1, true);
                 // Send SetBorder packet first
                 var c = this.gameServer.config;
-                this.socket.sendPacket(new Packet.SetBorder(c.borderLeft, c.borderRight, c.borderTop, c.borderBottom));
+                this.socket.sendPacket(new Packet.SetBorder(
+                        c.borderLeft + this.socket.playerTracker.scrambleX,
+                        c.borderRight + this.socket.playerTracker.scrambleX,
+                        c.borderTop + this.socket.playerTracker.scrambleY,
+                        c.borderBottom + this.socket.playerTracker.scrableY
+                ));
             }
             break;
         default:
