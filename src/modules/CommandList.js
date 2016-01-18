5027e722ce62c03bbd1bd32c8ea28222dfcc1af8
Andrew54757 <andrews54757@gmail.com>
2016-01-17 22:04:34 -0500

    Update CommandList.js

--- src/modules/CommandList.js
+++ src/modules/CommandList.js
@@ -174,6 +174,7 @@ Commands.list = {
      dop: function(gameServer,split) {
          var ops = parseInt(split[1]);
          gameServer.op[ops] = 0;
+         gameServer.opc[ops] = 0;
     console.log("De opped " + ops);
      },
         
