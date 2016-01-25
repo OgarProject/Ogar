26b3f26a5b3f034536d659f30d96437be0345e01
Andrew54757 <andrews54757@gmail.com>
2016-01-24 20:49:35 -0500

    Update CommandList.js

--- src/modules/CommandList.js
+++ src/modules/CommandList.js
@@ -1,6 +1,6 @@
 
 // Imports
-
+var Teams = require('../gamemodes/Teams.js');
 var GameMode = require('../gamemodes');
 var Entity = require('../entity');
 
@@ -88,9 +88,54 @@ Commands.list = {
         console.log("[Console] Clearban   : Resets Ban list");
         console.log("[Console] Resetvirus: Turns special viruses (from op's) into normal ones");
         console.log("[Console] Split      : Splits a player");
+        console.log("[Console] Team       : Changes a players Team");
         console.log("[Console] ====================================================");
     },
-    
+    team: function(gameServer,split) {
+        var id = parseInt(split[1]);
+        var team = split[2];
+        var tteam = 0;
+        var colors = [
+        {'r': 223, 'g': 0, 'b': 0},
+        {'r': 0, 'g': 223, 'b': 0},
+        {'r': 0, 'g': 0, 'b': 223},
+    ];
+        if (team) {
+            if (team == "r") {
+               tteam = 0;    
+            }
+            if (team == "g") {
+               tteam = 1;    
+            }
+            if (team == "b") {
+               tteam = 2;    
+            }
+            
+        } else {
+         console.log("Please specify a team (r,g,b)");
+            return;
+        }
+        if (isNaN(id)) { 
+             console.log("[Console] Please specify a valid player ID!"); 
+             return; 
+         } 
+ 
+         for (var i in gameServer.clients) { 
+             if (gameServer.clients[i].playerTracker.pID == id) { 
+                 var client = gameServer.clients[i].playerTracker; 
+                 
+                 client.team = tteam;
+                 client.setColor(colors[tteam]); // Set color
+                for (var j in client.cells) {
+                    client.cells[j].setColor(colors[tteam]);
+                }
+               break;
+                 console.log("Successivly changed the players team");
+             } 
+         } 
+        
+    },
+
     split: function(gameServer,split) {
 		// Validation checks
 		var id = parseInt(split[1]);
