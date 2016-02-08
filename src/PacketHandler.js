7509afa33af9d8374c4d3d79670541ba578174f7
Luka967 <luka.djokovic2@gmail.com>
2016-02-08 22:44:06 +0100

    Remove switchSpectator(...)
    Function removed from GameServer.js

--- src/PacketHandler.js
+++ src/PacketHandler.js
@@ -59,7 +59,6 @@ PacketHandler.prototype.handleMessage = function(message) {
             // Spectate mode
             if (this.socket.playerTracker.cells.length <= 0) {
                 // Make sure client has no cells
-                this.gameServer.switchSpectator(this.socket.playerTracker);
                 this.socket.playerTracker.spectate = true;
             }
             break;
