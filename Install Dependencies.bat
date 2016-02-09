53d960893b13236247a8b6526f219fdd834d0422
Luka967 <luka.djokovic2@gmail.com>
2016-02-09 01:05:16 +0100

    Added batch file for installing dependencies for Windows

---
+++ Install Dependencies.bat
@@ -0,0 +1,8 @@
+@echo off
+npm install
+echo.
+echo.
+echo Successfully installed all dependecies!
+echo Please don't touch folder "node-modules".
+echo It is the folder where dependecies are installed.
+pause >nul
