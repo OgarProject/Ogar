5773d1b301d8b48fa7ea6341c546c05860914ea1
Tiberiu <musattiberiu@gmail.com>
2016-02-10 18:25:01 +0200

    Update CommandList.js

--- src/modules/CommandList.js
+++ src/modules/CommandList.js
@@ -28,7 +28,6 @@ var fillChar = function(data, char, fieldLength, rTL) {
 Commands.list = {
     help: function(gameServer, split) {
         console.log("[Console] ======================== HELP ======================");
-        console.log("[Console] usage                        : see resorces used by the server");
         console.log("[Console] addbot [number]              : add bot to the server");
         console.log("[Console] kickbot [number]             : kick an amount of bots");
         console.log("[Console] board [string] [string] ...  : set scoreboard text");
@@ -445,11 +444,22 @@ Commands.list = {
                 bots++;
             }
         }
-        //
+
+	usage.lookup(process.pid, function(err, result) {
+	  // By unknown reason the usage will pe printed after that '>', so clear the line
+	  process.stdout.clearLine();
+	  process.stdout.cursorTo(0);
+
+          console.log("[Console] Current memory usage: " + Math.floor(result.memory / 10000) / 100 + "MB");
+	  console.log("[Console] Current CPU load: " + Math.floor(result.cpu) + "\%");
+
+          process.stdout.write(">");
+        });
+
         console.log("[Console] Connected players: " + gameServer.clients.length + "/" + gameServer.config.serverMaxConnections);
         console.log("[Console] Players: " + humans + " - Bots: " + bots);
         console.log("[Console] Server has been running for " + process.uptime() + " seconds");
-        console.log("[Console] Current memory usage: " + process.memoryUsage().heapUsed / 1000 + "/" + process.memoryUsage().heapTotal / 1000 + " kb");
+        
         console.log("[Console] Current game mode: " + gameServer.gameMode.name);
     },
     tp: function(gameServer, split) {
@@ -504,17 +514,5 @@ Commands.list = {
         gameServer.addNode(v);
         console.log("[Console] Spawned 1 virus at (" + pos.x + " , " + pos.y + ")");
     },
-    usage: function (gameServer, split, app){
-      // Get process pid
-      var pid = process.pid;
-	
-      usage.lookup(pid, function(err, result) {
-	// By unknown reason the status will pe printed after that '>', so clear the line
-	process.stdout.clearLine();
-	process.stdout.cursorTo(0);
-
-    	// Print status
-	process.stdout.write("[Console]" + " MEMORY: " + Math.floor(result.memory / 10000) / 100 + "MB, CPU: " + Math.floor(result.cpu) + "\%\n>");
-      });
 }
 };
