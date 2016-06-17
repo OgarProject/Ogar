c2c59410946e748e41ba1797463c9546f7b33986
Luka967 <luka.djokovic2@gmail.com>
2016-06-17 23:26:35 +0200

    Create PlayerHandler.js

---
+++ src/PlayerHandler.js
@@ -0,0 +1,21 @@
+function PlayerHandler(gameServer) {
+    this.gameServer = gameServer;
+}
+
+module.exports = PlayerHandler;
+
+PlayerHandler.prototype.update = function() {
+    // List through all clients and check if update is needed
+    for (var i = 0; i < this.gameServer.clients.length; i++) {
+        var client = this.gameServer.clients[i];
+        if (!client) continue;
+        
+        client.mapUpdate--;
+        if (client.mapUpdate > 0) continue;
+        client.mapUpdate = 40;
+        
+        // Update client
+        client.playerTracker.update();
+        client.playerTracker.antiTeamTick();
+    }
+};
