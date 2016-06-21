c86c4e1eb222832f1bb2ce78491d465fbf1be937
Luka967 <luka.djokovic2@gmail.com>
2016-06-21 22:32:46 +0200

    Support for main tick

--- src/PlayerHandler.js
+++ src/PlayerHandler.js
@@ -1,18 +1,27 @@
 function PlayerHandler(gameServer) {
     this.gameServer = gameServer;
+    this.tick = 0;
 }
 
 module.exports = PlayerHandler;
 
 PlayerHandler.prototype.update = function() {
     // List through all clients and check if update is needed
+    if (this.gameServer.serverLoadSpread < 0) {
+        this.tick--;
+        if (this.tick > 0) return;
+        this.tick = 40;
+    }
+    
     for (var i = 0; i < this.gameServer.clients.length; i++) {
         var client = this.gameServer.clients[i];
         if (!client) continue;
         
-        client.mapUpdate--;
-        if (client.mapUpdate > 0) continue;
-        client.mapUpdate = 40;
+        if (this.gameServer.serverLoadSpread >= 1) {
+            client.playerTracker.mapUpdate--;
+            if (client.mapUpdate > 0) continue;
+            client.playerTracker.mapUpdate = 40;
+        }
         
         // Update client
         client.playerTracker.update();
