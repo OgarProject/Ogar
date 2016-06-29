e3b738fda68ee0f3addbf1f93a915da6dce3cc88
Barbosik <barbos4agar@gmail.com>
2016-06-29 20:01:09 +0200

    performance improvement & reduce lags

--- src/packet/UpdateLeaderboard.js
+++ src/packet/UpdateLeaderboard.js
@@ -12,15 +12,24 @@ module.exports = UpdateLeaderboard;
 
 UpdateLeaderboard.prototype.build = function (protocol) {
     switch (this.leaderboardType) {
-        case 48: return this.build48(protocol); // UserText
-        case 49: return this.build49(protocol); // FFA
-        case 50: return this.build50(protocol); // Team
-        default: return null;
+        case 48:
+            // UserText
+            return this.buildUserText(protocol);
+        case 49:
+            // FFA
+            if (protocol < 6)
+                return this.buildFfa5();
+            return this.buildFfa6();
+        case 50:
+            // Team
+            return this.buildTeam();
+        default:
+            return null;
     }
 }
 
 // UserText
-UpdateLeaderboard.prototype.build48 = function (protocol) {
+UpdateLeaderboard.prototype.buildUserText = function (protocol) {
     var writer = new BinaryWriter();
     writer.writeUInt8(0x31);                                // Packet ID
     writer.writeUInt32(this.leaderboard.length >>> 0);       // Number of elements
@@ -41,8 +50,8 @@ UpdateLeaderboard.prototype.build48 = function (protocol) {
     return writer.toBuffer();
 };
 
-// (FFA) Leaderboard Update
-UpdateLeaderboard.prototype.build49 = function (protocol) {
+// FFA protocol 5
+UpdateLeaderboard.prototype.buildFfa5 = function () {
     var player = this.playerTracker;
     if (player.spectate && player.spectateTarget != null) {
         player = player.spectateTarget;
@@ -54,33 +63,55 @@ UpdateLeaderboard.prototype.build49 = function (protocol) {
         var item = this.leaderboard[i];
         if (item == null) return null;  // bad leaderboardm just don't send it
 
-        var name = item.getName();
-        name = name != null ? name : "";
-        var id = item == player ? 1 : 0;
-        if (id && protocol < 6 && item.cells.length > 0) {
-            // protocol 5- uses player cellId instead of isMe flag
+        var name = item.getNameUnicode();
+        var id = 0;
+        if (item == player && item.cells.length > 0) {
             id = item.cells[0].nodeId ^ this.playerTracker.scrambleId;
         }
 
-        writer.writeUInt32(id >>> 0);                        // isMe flag/cell ID
-        if (protocol <= 5)
-            writer.writeStringZeroUnicode(name);
+        writer.writeUInt32(id >>> 0);   // Player cell Id
+        if (name != null)
+            writer.writeBytes(name);
         else
-            writer.writeStringZeroUtf8(name);
+            writer.writeUInt16(0);
     }
     return writer.toBuffer();
 };
 
-// (Team) Leaderboard Update
-UpdateLeaderboard.prototype.build50 = function (protocol) {
+// FFA protocol 6
+UpdateLeaderboard.prototype.buildFfa6 = function () {
+    var player = this.playerTracker;
+    if (player.spectate && player.spectateTarget != null) {
+        player = player.spectateTarget;
+    }
     var writer = new BinaryWriter();
-    writer.writeUInt8(0x32);                                // Packet ID
+    writer.writeUInt8(0x31);                                // Packet ID
     writer.writeUInt32(this.leaderboard.length >>> 0);       // Number of elements
     for (var i = 0; i < this.leaderboard.length; i++) {
         var item = this.leaderboard[i];
         if (item == null) return null;  // bad leaderboardm just don't send it
         
-        var value = item;
+        var name = item.getNameUtf8();
+        var id = item == player ? 1 : 0;
+        
+        writer.writeUInt32(id >>> 0);   // isMe flag
+        if (name != null)
+            writer.writeBytes(name);
+        else
+            writer.writeUInt8(0);
+    }
+    return writer.toBuffer();
+};
+
+// Team
+UpdateLeaderboard.prototype.buildTeam = function () {
+    var writer = new BinaryWriter();
+    writer.writeUInt8(0x32);                                // Packet ID
+    writer.writeUInt32(this.leaderboard.length >>> 0);       // Number of elements
+    for (var i = 0; i < this.leaderboard.length; i++) {
+        var value = this.leaderboard[i];
+        if (value == null) return null;  // bad leaderboardm just don't send it
+        
         if (isNaN(value)) value = 0;
         value = value < 0 ? 0 : value;
         value = value > 1 ? 1 : value;
