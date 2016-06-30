9b1fe8ca51692449339f490ffd80b28b3af41fa3
Barbosik <barbos4agar@gmail.com>
2016-06-30 16:31:12 +0200

    protocol optimizations

--- src/packet/SetBorder.js
+++ src/packet/SetBorder.js
@@ -11,18 +11,27 @@ function SetBorder(playerTracker, border, gameType, serverName) {
 
 module.exports = SetBorder;
 
-SetBorder.prototype.build = function(protocol) {
+SetBorder.prototype.build = function (protocol) {
+    var scrambleX = this.playerTracker.scrambleX;
+    var scrambleY = this.playerTracker.scrambleY;
+    if (this.gameType == null) {
+        var buffer = new Buffer(33);
+        buffer.writeUInt8(0x40, 0, true);
+        buffer.writeDoubleLE(this.border.minx + scrambleX, 1, true);
+        buffer.writeDoubleLE(this.border.miny + scrambleY, 9, true);
+        buffer.writeDoubleLE(this.border.maxx + scrambleX, 17, true);
+        buffer.writeDoubleLE(this.border.maxy + scrambleY, 25, true);
+        return buffer;
+    }
     var writer = new BinaryWriter();
     writer.writeUInt8(0x40);                                // Packet ID
-    writer.writeDouble(this.border.minx + this.playerTracker.scrambleX);
-    writer.writeDouble(this.border.miny + this.playerTracker.scrambleY);
-    writer.writeDouble(this.border.maxx + this.playerTracker.scrambleX);
-    writer.writeDouble(this.border.maxy + this.playerTracker.scrambleY);
-    if (this.gameType != null) {
-        writer.writeUInt32(this.gameType >> 0);
-        var name = this.serverName;
-        if (name == null) name = "";
-        writer.writeStringZeroUtf8(name);
-    }
+    writer.writeDouble(this.border.minx + scrambleX);
+    writer.writeDouble(this.border.miny + scrambleY);
+    writer.writeDouble(this.border.maxx + scrambleX);
+    writer.writeDouble(this.border.maxy + scrambleY);
+    writer.writeUInt32(this.gameType >> 0);
+    var name = this.serverName;
+    if (name == null) name = "";
+    writer.writeStringZeroUtf8(name);
     return writer.toBuffer();
 };
