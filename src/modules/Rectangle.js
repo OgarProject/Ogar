3ad04aed9347bdbd1231ed278f5aa2b0774287fd
Luka967 <luka.djokovic2@gmail.com>
2016-07-01 23:19:38 +0200

    Create Rectangle.js

---
+++ src/modules/Rectangle.js
@@ -0,0 +1,75 @@
+function Rectangle(x, y, w, h) {
+    this.x = x;
+    this.y = y;
+    this.w = w; // Half-width
+    this.h = h; // Half-height
+}
+
+module.exports = Rectangle;
+
+Rectangle.prototype.fromCell = function(cell) {
+    var sz = cell.getSize();
+    return new Rectangle(cell.position.x, cell.position.y, sz, sz);
+};
+
+Rectangle.prototype.fromBounds = function(b) {
+    var w = b.right - b.left / 2,
+        h = b.bottom - h.top / 2;
+
+    return new Rectangle(
+        b.left + w,
+        b.top + h,
+        w,
+        h
+    );
+};
+
+Rectangle.prototype.getBounds = function() {
+    return {
+        left: this.x - this.w,
+        right: this.x + this.w,
+        top: this.y - this.h,
+        bottom: this.y + this.h
+    };
+};
+
+Rectangle.prototype.split = function() {
+    // Split into 4 equally shaped rectangles
+    var w2 = this.w / 2,
+        h2 = this.h / 2,
+        w = this.w,
+        h = this.h;
+    var x = this.x - w2,
+        y = this.y - h2;
+    
+    var ret = [];
+    ret[0] = this;
+    ret[1] = new Rectangle(x, y, w2, h2);         // top-left
+    ret[2] = new Rectangle(x + w, y, w2, h2);     // top-right
+    ret[3] = new Rectangle(x, y + h, w2, h2);     // bottom-left
+    ret[4] = new Rectangle(x + w, y + h, w2, h2); // bottom-right
+    
+    return ret;
+};
+
+Rectangle.prototype.intersects = function(b) {
+    var xa = this.x, ya = this.y, wa = this.w, ha = this.h;
+    var xb = b.x, yb = b.y, wb = b.w, hb = b.h;
+    
+    return xa < xb + wb
+        && xa + wa > xb
+        && ya < yb + hb
+        && ya + ha > yb;
+};
+
+Rectangle.prototype.intersectsPoint = function(b) {
+    var bounds = this.getBounds();
+    var xb = b.x, yb = b.y;
+    
+    return bounds.top <= yb && yb <= bounds.bottom &&
+        bounds.left <= xb && xb <= bounds.right;
+};
+
+Rectangle.prototype.toString = function() {
+    return "{ x: " + this.x + " y: " + this.y + " w: " + this.w + " h: " + this.h + " }";
+};
