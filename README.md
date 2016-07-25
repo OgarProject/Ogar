69f4428f7d796d40bb785e2c0517216d9cccca49
Barbosik <barbos4agar@gmail.com>
2016-07-25 20:17:18 +0200

    protocol optimizations / reduce lags / better protocol validation

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 ﻿# MultiOgar
 Ogar game server with fast and smooth vanilla physics and multi-protocol support.
 
-Current version: **1.2.53**
+Current version: **1.2.54**
 
 ## Project Info
 ![Language](https://img.shields.io/badge/language-node.js-yellow.svg)
--- src/PacketHandler.js
+++ src/PacketHandler.js
@@ -6,8 +6,11 @@ function PacketHandler(gameServer, socket) {
     this.gameServer = gameServer;
     this.socket = socket;
     this.protocol = 0;
-    this.isHandshakePassed = false;
+    this.handshakeProtocol = null;
+    this.handshakeKey = null;
     this.lastChatTick = 0;
+    this.lastJoinTick = 0;
+    this.lastMouseTick = 0;
     
     this.pressQ = false;
     this.pressW = false;
@@ -27,37 +30,8 @@ PacketHandler.prototype.handleMessage = function (message) {
         this.socket.close(1009, "Spam");
         return;
     }
-    
-    // no handshake?
-    if (!this.isHandshakePassed) {
-        if (message[0] != 254 || message.length != 5) {
-            // wait handshake
-            return;
-        }
-        
-        var reader = new BinaryReader(message);
-        reader.skipBytes(1);
-        
-        // Handshake request
-        this.protocol = reader.readUInt32();
-        if (this.protocol < 1 || this.protocol > 8) {
-            this.socket.close(1002, "Not supported protocol");
-            return;
-        }
-        // Send handshake response
-        this.socket.sendPacket(new Packet.ClearAll());
-        this.socket.sendPacket(new Packet.SetBorder(this.socket.playerTracker, this.gameServer.border, this.gameServer.config.serverGamemode, "MultiOgar " + pjson.version));
-        // Send welcome message
-        this.gameServer.sendChatMessage(null, this.socket.playerTracker, "MultiOgar " + pjson.version);
-        if (this.gameServer.config.serverWelcome1)
-            this.gameServer.sendChatMessage(null, this.socket.playerTracker, this.gameServer.config.serverWelcome1);
-        if (this.gameServer.config.serverWelcome2)
-            this.gameServer.sendChatMessage(null, this.socket.playerTracker, this.gameServer.config.serverWelcome2);
-        if (this.gameServer.config.serverChat == 0)
-            this.gameServer.sendChatMessage(null, this.socket.playerTracker, "This server's chat is disabled.");
-        if (this.protocol < 4)
-            this.gameServer.sendChatMessage(null, this.socket.playerTracker, "WARNING: Protocol " + this.protocol + " assumed as 4!");
-        this.isHandshakePassed = true;
+    if (this.handshakeProtocol == null || this.handshakeKey == null) {
+        this.handleHandshake(message);
         return;
     }
     this.socket.lastAliveTime = +new Date;
@@ -66,14 +40,17 @@ PacketHandler.prototype.handleMessage = function (message) {
         return;
     }
     
-    var reader = new BinaryReader(message);
-    var packetId = reader.readUInt8();
-    
-    switch (packetId) {
+    switch (message[0]) {
         case 0:
+            if (this.lastJoinTick == this.gameServer.getTick()) {
+                break;
+            }
+            this.lastJoinTick = this.gameServer.getTick();
             if (this.socket.playerTracker.cells.length > 0) {
                 break;
             }
+            var reader = new BinaryReader(message);
+            reader.skipBytes(1);
             var text = null;
             if (this.protocol <= 5)
                 text = reader.readStringZeroUnicode();
@@ -89,19 +66,31 @@ PacketHandler.prototype.handleMessage = function (message) {
                 this.socket.playerTracker.spectate = true;
             }
             break;
+        
         case 16:
             // Mouse
+            if (this.lastMouseTick == this.gameServer.getTick()) {
+                break;
+            }
+            this.lastMouseTick = this.gameServer.getTick();
+            
             var client = this.socket.playerTracker;
             if (message.length == 13) {
                 // protocol late 5, 6, 7
+                var reader = new BinaryReader(message);
+                reader.skipBytes(1);
                 client.mouse.x = reader.readInt32() - client.scrambleX;
                 client.mouse.y = reader.readInt32() - client.scrambleY;
             } else if (message.length == 9) {
                 // early protocol 5
+                var reader = new BinaryReader(message);
+                reader.skipBytes(1);
                 client.mouse.x = reader.readInt16() - client.scrambleX;
                 client.mouse.y = reader.readInt16() - client.scrambleY;
             } else if (message.length == 21) {
                 // protocol 4
+                var reader = new BinaryReader(message);
+                reader.skipBytes(1);
                 client.mouse.x = reader.readDouble() - client.scrambleX;
                 client.mouse.y = reader.readDouble() - client.scrambleY;
                 if (isNaN(client.mouse.x))
@@ -110,21 +99,26 @@ PacketHandler.prototype.handleMessage = function (message) {
                     client.mouse.y = client.centerPos.y;
             }
             break;
+        
         case 17:
             // Space Press - Split cell
             this.pressSpace = true;
             break;
+        
         case 18:
             // Q Key Pressed
             this.pressQ = true;
             break;
+        
         case 19:
             // Q Key Released
             break;
+        
         case 21:
             // W Press - Eject mass
             this.pressW = true;
             break;
+        
         case 99:
             // Chat
             if (message.length < 3)             // first validation
@@ -138,11 +132,14 @@ PacketHandler.prototype.handleMessage = function (message) {
             if (deltaTick < 40 * 2)
                 break;
             
-            var flags = reader.readUInt8();    // flags
+            var flags = message[1];    // flags
             var rvLength = (flags & 2 ? 4:0) + (flags & 4 ? 8:0) + (flags & 8 ? 16:0);
             if (message.length < 3 + rvLength) // second validation
                 break;
-            reader.skipBytes(rvLength);        // reserved
+            
+            var reader = new BinaryReader(message);
+            reader.skipBytes(2 + rvLength);     // reserved
+            
             var text = null;
             if (this.protocol < 6)
                 text = reader.readStringZeroUnicode();
@@ -150,6 +147,7 @@ PacketHandler.prototype.handleMessage = function (message) {
                 text = reader.readStringZeroUtf8();
             this.gameServer.onChatMessage(this.socket.playerTracker, null, text);
             break;
+        
         case 254:
             // Server stat
             var time = +new Date;
@@ -158,9 +156,60 @@ PacketHandler.prototype.handleMessage = function (message) {
             if (dt < 1000) break;
             this.socket.sendPacket(new Packet.ServerStat(this.socket.playerTracker));
             break;
+        
+        case 255:
+            if (message.length != 5) {
+                this.socket.close(1002, "Not supported protocol");
+                break;
+            }
+            if (this.protocol > 6) {
+                var key = message[1] | (message[2] << 8) | (message[3] << 16) | (message[4] << 24);
+                if (key != 0) {
+                    this.socket.close(1002, "Not supported protocol");
+                }
+            }
+            break;
+
         default:
             break;
     }
+};
+
+PacketHandler.prototype.handleHandshake = function (message) {
+    if (message.length != 5) {
+        this.socket.close(1002, "Invalid protocol");
+        return;
+    }
+    if (message[0] == 254) {
+        this.handshakeProtocol = message[1] | (message[2] << 8) | (message[3] << 16) | (message[4] << 24);
+        if (this.handshakeProtocol < 1 || this.handshakeProtocol > 8) {
+            this.socket.close(1002, "Not supported protocol");
+        }
+    }
+    if (message[0] == 255) {
+        this.handshakeKey = message[1] | (message[2] << 8) | (message[3] << 16) | (message[4] << 24);
+        if (this.handshakeProtocol > 6 && this.handshakeKey != 0) {
+            this.socket.close(1002, "Not supported protocol");
+        }
+    }
+    if (this.handshakeProtocol == null || this.handshakeKey == null) {
+        return;
+    }
+    this.protocol = this.handshakeProtocol;
+
+    // Send handshake response
+    this.socket.sendPacket(new Packet.ClearAll());
+    this.socket.sendPacket(new Packet.SetBorder(this.socket.playerTracker, this.gameServer.border, this.gameServer.config.serverGamemode, "MultiOgar " + pjson.version));
+    // Send welcome message
+    this.gameServer.sendChatMessage(null, this.socket.playerTracker, "MultiOgar " + pjson.version);
+    if (this.gameServer.config.serverWelcome1)
+        this.gameServer.sendChatMessage(null, this.socket.playerTracker, this.gameServer.config.serverWelcome1);
+    if (this.gameServer.config.serverWelcome2)
+        this.gameServer.sendChatMessage(null, this.socket.playerTracker, this.gameServer.config.serverWelcome2);
+    if (this.gameServer.config.serverChat == 0)
+        this.gameServer.sendChatMessage(null, this.socket.playerTracker, "This server's chat is disabled.");
+    if (this.protocol < 4)
+        this.gameServer.sendChatMessage(null, this.socket.playerTracker, "WARNING: Protocol " + this.protocol + " assumed as 4!");
 };
 
 PacketHandler.prototype.setNickname = function (text) {
