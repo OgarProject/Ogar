cd6171ca1127d9c33c04fadbaae897712f375ccd
Luka967 <luka.djokovic2@gmail.com>
2016-07-28 00:42:03 +0200

    Use the new scramblers

--- src/packet/UpdateNodes.js
+++ src/packet/UpdateNodes.js
@@ -1,10 +1,12 @@
 var DynamicBuffer = require('./DynamicBuffer');
 
-function UpdateNodes(nodes, nonVisibleNodes, scrambleX, scrambleY, protocolVersion) {
+function UpdateNodes(nodes, nonVisibleNodes, sender, protocolVersion) {
     this.nodes = nodes;
     this.nonVisibleNodes = nonVisibleNodes;
-    this.scrambleX = scrambleX;
-    this.scrambleY = scrambleY;
+    this.scrambleX = sender.scrambleX;
+    this.scrambleY = sender.scrambleY;
+    this.scrambleID = sender.scrambleID;
+    this.scrambleColor = sender.scrambleColor;
     this.protocolVersion = protocolVersion;
 }
 
@@ -26,9 +28,9 @@ UpdateNodes.prototype.build = function() {
     for (var i = 0; i < deadCells.length; i++) {
         var node = deadCells[i];
         var id = 0;
-        if (node.getKiller()) id = node.getKiller().nodeId;
+        if (node.getKiller()) id = node.getKiller().nodeId ^ this.scrambleID;
         buffer.setUint32(id);                                                   // Eaten ID
-        buffer.setUint32(node.nodeId);                                          // Eater ID
+        buffer.setUint32(node.nodeId ^ this.scrambleID);                        // Eater ID
     }
 
     for (var i = 0; i < this.nodes.length; i++) {                               // Update nodes
@@ -36,23 +38,31 @@ UpdateNodes.prototype.build = function() {
         if (!node) continue; // Error!
 
         if (node.nodeId == 0) continue; // Error!
-        buffer.setUint32(node.nodeId);                                          // Node ID
+        buffer.setUint32(node.nodeId ^ this.scrambleID);                        // Node ID
         buffer.setInt32(node.position.x + this.scrambleX);                      // Node's X pos
         buffer.setInt32(node.position.y + this.scrambleY);                      // Node's Y pos
         buffer.setUint16(node.getSize());                                       // Node size
 
         var flags = 0;
+        var colors = [node.color.r, node.color.g, node.color.b];
 
-        if (this.protocolVersion != 5) {
+        if (this.scrambleColor.from != -1 && this.scrambleColor.to != -1 && (node.cellType == 0 || node.cellType == 3)) {
+            var from = colors[this.scrambleColor.from],
+                to = colors[this.scrambleColor.to];
+            colors[this.scrambleColor.from] = to;
+            colors[this.scrambleColor.to] = from;
+        }
+
+        if (this.protocolVersion >= 6) {
             // Flags
             if (node.getName() != null && node.getName() != "") flags += 8;
             flags += 2;
             if (node.spiked) flags += 1;
 
             buffer.setUint8(flags);                                             // Node's update flags
-            buffer.setUint8(node.color.r);                                      // Node's R color
-            buffer.setUint8(node.color.g);                                      // Node's G color
-            buffer.setUint8(node.color.b);                                      // Node's B color
+            buffer.setUint8(colors[0]);                                         // Node's R color
+            buffer.setUint8(colors[1]);                                         // Node's G color
+            buffer.setUint8(colors[2]);                                         // Node's B color
             if (node.getName() != null && node.getName() != "") {
                 buffer.setStringUTF8(node.getName());                           // Node's name
                 buffer.setUint8(0);                                             // Node name terminator
@@ -61,9 +71,9 @@ UpdateNodes.prototype.build = function() {
             // Flags
             if (node.spiked) flags += 1;
 
-            buffer.setUint8(node.color.r);                                      // Node's R color
-            buffer.setUint8(node.color.g);                                      // Node's G color
-            buffer.setUint8(node.color.b);                                      // Node's B color
+            buffer.setUint8(colors[0]);                                         // Node's R color
+            buffer.setUint8(colors[1]);                                         // Node's G color
+            buffer.setUint8(colors[2]);                                         // Node's B color
             buffer.setUint8(flags);                                             // Node's update flags
             if (node.getName() != null && node.getName() != "") {
                 buffer.setStringUnicode(node.getName());                        // Node's name
@@ -73,14 +83,14 @@ UpdateNodes.prototype.build = function() {
     }
     buffer.setUint32(0);                                                        // Update nodes end
 
-    if (this.protocolVersion != 5) {
+    if (this.protocolVersion >= 6) {
         buffer.setUint16(this.nonVisibleNodes.length);                          // Remove actions length
     } else {
         buffer.setUint32(this.nonVisibleNodes.length);                          // Remove actions length
     }
     for (var i = 0; i < this.nonVisibleNodes.length; i++) {
         if (!this.nonVisibleNodes[i]) continue;
-        buffer.setUint32(this.nonVisibleNodes[i].nodeId);                       // Removing node's ID
+        buffer.setUint32(this.nonVisibleNodes[i].nodeId ^ this.scrambleID);     // Removing node's ID
     }
 
     return buffer.build();
