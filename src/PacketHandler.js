24f28c7ba46ae11db6feac396551dba37976ee1d
Barbosik <barbos4agar@gmail.com>
2016-08-10 19:23:46 +0200

    add support for protocol 9

--- src/PacketHandler.js
+++ src/PacketHandler.js
@@ -38,7 +38,7 @@ PacketHandler.prototype.handleMessage = function (message) {
 PacketHandler.prototype.handshake_onProtocol = function (message) {
     if (message.length !== 5) return;
     this.handshakeProtocol = message[1] | (message[2] << 8) | (message[3] << 16) | (message[4] << 24);
-    if (this.handshakeProtocol < 1 || this.handshakeProtocol > 8) {
+    if (this.handshakeProtocol < 1 || this.handshakeProtocol > 9) {
         this.socket.close(1002, "Not supported protocol");
         return;
     }
