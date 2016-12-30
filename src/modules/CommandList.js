ab865a5ddbfca0b7947770b84d42ea3d71fd09b4
Tyler3D <Tylermanrique@gmail.com>
2016-12-30 01:39:51 -0500

    Gamemode command doesn't work well

--- src/modules/CommandList.js
+++ src/modules/CommandList.js
@@ -329,23 +329,6 @@ function Commands() {
             desc: "Spawns a food pellet in the game.",
             vars: "<x> <y> [mass]"
         },
-        gamemode: {
-            f: function(gameServer, split) {
-                try {
-                    var n = parseInt(split[1]);
-                    var gm = gameServer.pluginHandler.gamemodes.retrieveGamemode(n); // If there is an invalid gamemode, the function will exit
-                    gameServer.gameMode.onChange(gameServer); // Reverts the changes of the old gamemode
-                    gameServer.gameMode = gm; // Apply new gamemode
-                    gameServer.gameMode.onServerInit(gameServer); // Resets the server
-                    console.log("[Game] Changed game mode to " + gameServer.gameMode.name);
-                } catch (e) {
-                    console.log("[Console] Invalid game mode selected");
-                }
-            },
-            name: "gamemode",
-            desc: "Changes the gamemode.",
-            vars: "<id>"
-        },
         kick: {
             f: function(gameServer, split) {
                 var id = parseInt(split[1]);
